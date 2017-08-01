803d5beeb5b104980268367c6d9fff24a311ecdc
Sherrod Blankner <mooncalf@Mooncalf.local>
2017-07-31 18:52:40 -0700

    adding NaN

--- week3.py
+++ week3.py
@@ -6,6 +6,7 @@
 
 ##### set up df
 import pandas as pd
+import numpy as np
 
 xl = pd.ExcelFile("Energy Indicators.xls", skiprows=18)
 xl.sheet_names
@@ -16,5 +17,10 @@ energy.drop(energy.columns[[0,1]], axis=1,inplace=True)
 energy.drop(energy.index[0:16], axis=0,inplace=True)
 #drop footer
 energy.drop(energy.index[227:], axis=0,inplace=True)
+#rename columns
 energy.rename(columns = {'Environmental Indicators: Energy':'Country', 'Unnamed: 3':'Energy Supply', 'Unnamed: 4':'Energy Supply per Capita', 'Unnamed: 5':'% Renewable'}, inplace = True)
+#convert NaNs
+energy.replace({'...': np.nan}, inplace=True)
+#convert to gigajoules
+energy['Energy Supply'] = energy['Energy Supply'].apply(lambda x: x*1000000)
 print(energy)
